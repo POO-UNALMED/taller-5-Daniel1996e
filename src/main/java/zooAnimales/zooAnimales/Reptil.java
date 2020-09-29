5ed704071ef344c7510927e3622124220602a69f
Daniel1996e <43450731+Daniel1996e@users.noreply.github.com>
2020-09-29 14:35:27 -0500

    taller-5-Daniel1996e

---
+++ src/main/java/zooAnimales/zooAnimales/Reptil.java
@@ -0,0 +1,71 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reptil extends Animal{
+	private static List<Reptil> listado= new ArrayList<Reptil> ();
+	public static int iguanas;
+	public static int serpientes;
+	private String colorEscamas;
+	private int largoCola;
+	
+	public Reptil(){
+		super();
+		increaseTotal();
+	}
+	
+	public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola){
+		this.setNombre(nombre);
+		this.setEdad(edad);
+		this.setHabitat(habitat);
+		this.setGenero(genero);
+		this.colorEscamas = colorEscamas;
+		this.largoCola = largoCola;
+	}
+	
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+	public int getLargoCola() {
+		return largoCola;
+	}
+	public void setLargoCola(int largoCola) {
+		this.largoCola = largoCola;
+	}
+	
+	public String movimiento() {
+		return "reptar";
+	}
+	
+	public void crearIguana(String nombre, int edad, String genero) {
+		this.setNombre(nombre);
+		this.setEdad(edad);
+		this.setHabitat("humedal");
+		this.setGenero(genero);
+		this.colorEscamas = "verde";
+		this.largoCola = 3 ;
+		iguanas ++;
+		increaseTotal();
+		listado.add(this);
+	}
+	
+	public void crearSerpiente(String nombre, int edad, String genero) {
+		this.setNombre(nombre);
+		this.setEdad(edad);
+		this.setHabitat("jungla");
+		this.setGenero(genero);
+		this.colorEscamas = "blanco";
+		this.largoCola = 1 ;
+		serpientes ++;
+		increaseTotal();
+		listado.add(this);
+	}
+	
+	public static int cantidadReptiles() {
+		return iguanas + serpientes + getTotalAnimales();
+	}
+}
